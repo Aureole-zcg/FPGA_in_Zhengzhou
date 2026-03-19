3130f37bb42ad2dc9e3acfcf4f56067fbce1815e
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2026-03-19 11:46:27 +0800

    Update mux4_1.v

--- mux4_1/mux4_1.v
+++ mux4_1/mux4_1.v
@@ -15,24 +15,24 @@ module mux4_1
 
 //第一种写法：assign+三目运算符
 //assign后只能跟一条语句
-//assign add_out=(sel==2'd00)? add_in0:
-//               (sel==2'd01)? add_in1:
-//               (sel==2'd10)? add_in2:add_in3;
+//assign add_out=(sel==2'b00)? add_in0:
+//               (sel==2'b01)? add_in1:
+//               (sel==2'b10)? add_in2:add_in3;
 
 //第二种写法：always+三目运算符
 //always @(*)//(sel,add_in0,add_in1,add_in2,add_in3)
-//    add_out=(sel==2'd00)? add_in0:
-//               (sel==2'd01)? add_in1:
-//               (sel==2'd10)? add_in2:add_in3;
+//    add_out=(sel==2'b00)? add_in0:
+//               (sel==2'b01)? add_in1:
+//               (sel==2'b10)? add_in2:add_in3;
 
 //第三种写法：always+if else
 //always @(*)
 //begin
-//    if(sel==2'd00)
+//    if(sel==2'b00)
 //        add_out=add_in0;
-//    else if(sel==2'd01)
+//    else if(sel==2'b01)
 //        add_out=add_in1;
-//    else if(sel==2'd10)
+//    else if(sel==2'b10)
 //        add_out=add_in2;
 //    else 
 //        add_out=add_in3;
@@ -42,9 +42,9 @@ module mux4_1
 always @(*)
 begin
     case (sel)
-        2'd00:add_out=add_in0;
-        2'd01:add_out=add_in1;
-        2'd10:add_out=add_in2;
+        2'b00:add_out=add_in0;
+        2'b01:add_out=add_in1;
+        2'b10:add_out=add_in2;
         default:add_out=add_in3;
     endcase
 end
