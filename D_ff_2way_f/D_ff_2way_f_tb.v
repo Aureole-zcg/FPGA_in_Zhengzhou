334356e221636a6eb30d6ffb7a6ecfb9da64ac07
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2026-03-20 09:27:22 +0800

    Clean up D_ff_2way_f_tb by removing D register
    Removed unused D register and related assignments.

--- D_ff_2way_f/D_ff_2way_f_tb.v
+++ D_ff_2way_f/D_ff_2way_f_tb.v
@@ -1,14 +1,13 @@
 `timescale 1ns/1ps
 
 module D_ff_2way_f_tb();
-reg D;
+
 reg clk;
 reg rst_n;
 wire Q;
 
 initial
 begin
-    D=1'b0;
     clk=1'b0;
     rst_n=1'b0;//系统复位
     #77
@@ -17,12 +16,9 @@ end
 
 //clk:50MHz  T=20ns
 always #10 clk=~clk;//获取周期稳定震荡的时钟信号
-always @(Q)  D<=~Q;
-
 
 D_ff_2way_f D_ff_2way_f_inst
 (
-    .D(D),
     .clk(clk),
     .rst_n(rst_n),
 
