66e8d2ffb862606d3c61a5ed0f93fb49b7da5add
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2026-03-20 15:04:13 +0800

    Add initial comments for flash_led module

---
+++ flash_led.v
@@ -0,0 +1,27 @@
+//目标：LED0.5s交替亮灭
+//使用EP4CE10F17C8，利用计数器和50MHz晶振，计数25000000个上升边沿
+module flash_led
+(
+    input wire clk,
+    input wire rst_n,
+
+    output reg Y
+);
+
+reg [31:0] cnt ;
+always @(posedge clk , negedge rst_n)
+begin
+    if(~rst_n)
+        cnt<=1'd0;
+    else if(cnt<49999999)
+        cnt<=cnt+1'd1;
+    else cnt<=1'd0;
+end 
+
+always @(*) 
+begin
+    if (cnt<=24999999) 
+        Y=1'b0;
+    else Y=1'b1;
+end
+endmodule
