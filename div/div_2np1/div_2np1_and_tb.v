781393f7107fbc410c787fee947e5b7806d8a253
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2026-03-23 22:16:14 +0800

    Create div_2np1_and_tb.v

---
+++ div/div_2np1/div_2np1_and_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+module div_2np1_and_tb();
+reg clk,rst_n;
+wire clk_div2np1;
+
+initial 
+begin
+    clk=1'b0;
+    rst_n=1'b0;
+    #77
+    rst_n=1'b1;
+end
+
+always #10 clk=~clk;
+
+div_2np1_and 
+#(
+   .DIV_NUM(32'd3)//3分频
+)
+div_2np1_and_inst7(
+    .clk(clk),
+    .rst_n(rst_n),
+    .clk_div2np1(clk_div2np1)
+);
+endmodule
+
