5584569f5a6c32f0e93e64fb0ccd2a1e47c62768
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2026-03-26 17:52:47 +0800

    Update breath_led_1and2s_top.v

--- breath_led/breath_led_1and2s_top.v
+++ breath_led/breath_led_1and2s_top.v
@@ -2,7 +2,7 @@ module breath_led_1and2s_top
 (
     input wire clk,rst_n,//50MHz，低电平复位
 
-    output wire led0, led1
+    output wire led0, led1, led2, led3
 );
 
 breath_led breath_led_inst1s
@@ -19,5 +19,21 @@ breath_led_2s breath_led_2s_inst2s
     .rst_n(rst_n),//50MHz，低电平复位
 
     .led_out(led1)
+);
+
+breath_led_4s_v1 breath_led_4s_v1_inst4s
+(
+    .clk(clk),
+    .rst_n(rst_n),//50MHz，低电平复位
+
+    .led_out(led2)
+);
+
+breath_led_4s_v2 breath_led_4s_v2_inst4s
+(
+    .clk(clk),
+    .rst_n(rst_n),//50MHz，低电平复位
+
+    .led_out(led3)
 );
 endmodule
