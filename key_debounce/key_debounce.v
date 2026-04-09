b8f1511c4ae5dc806ddf77a9bbaa678d710c3b76
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2026-04-09 17:11:49 +0800

    Update key_debounce.v

--- key_debounce/key_debounce.v
+++ key_debounce/key_debounce.v
@@ -17,7 +17,7 @@ begin
     else if (key_in==1'b1)
             cnt<=32'd0;
         else if (cnt==CNT_MAX)
-                cnt<=cnt;
+                cnt<=cnt;//维持，以确保长按不会重复触发
             else cnt<=cnt+1'b1;    
 end
 
@@ -25,7 +25,7 @@ always @(posedge clk, negedge rst_n)
 begin
     if (~rst_n)
         key_out<=1'b0;
-    else if (cnt==CNT_MAX-1)//最后一位计数保持，以控制按下次数
+    else if (cnt==CNT_MAX-1)//最后一位计数保持，以控制按下次数，倒数第二位数只出现一次，来做标志位
             key_out<=1'b1;//单独高电平表示采集到按键按下，按键标示电平
         else key_out<=1'b0;
 end
